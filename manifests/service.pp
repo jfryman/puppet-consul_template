07a0bf81bade26260cb342c02186d7c1b6b08315
James Fryman <james@fryman.io>
2015-12-24 17:23:40 -0600

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,7 +3,9 @@ class consul_template::service {
   $_service_enable = $consul_template::service_enable
 
   service { 'consul_template':
-    ensure => $_service_ensure,
-    enable => $_service_enable,
+    ensure     => $_service_ensure,
+    enable     => $_service_enable,
+    hasstatus  => true,
+    hasrestart => true,
   }
 }
